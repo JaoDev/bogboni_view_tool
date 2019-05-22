aced80d5ca6ed07bfab443b110bf77e89766501d
Humsbel Jao <humsbel_obregon12@hotmail.com>
2019-05-21 19:54:51 -0700

    Dev tool initial commit copyright

---
+++ lib/bogboni_view_tool.rb
@@ -0,0 +1,6 @@
+require "bogboni_view_tool/version"
+require "bogboni_view_tool/renderer"
+
+module BogboniViewTool
+  # Your code goes here...
+end
---
+++ lib/bogboni_view_tool/version.rb
@@ -0,0 +1,3 @@
+module BogboniViewTool
+  VERSION = "0.1.0"
+end
